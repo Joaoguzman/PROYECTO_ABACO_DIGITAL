fe8878b760558db1a82921b55c910237bd3a5f4e
Pedro <pedrocid@example.com>
2020-11-13 10:42:38 -0300

    primer esquema del proyecto

---
+++ abaco.py
@@ -0,0 +1,31 @@
+'''
+Haga un programa que muestre una cantidad ingresada por el usuario en
+forma de abaco en pantalla
+
+** Use buenas practicas de programación en python pep8
+'''
+#funcion que valida la entrada del usuario aceptando
+#solo digitos y no caracteres.
+#debe retornar el numero o string que ingresó el usuario
+def solo_digitos():
+    
+#funcion que toma el numero ingresado por el usuario y lo 
+# descompone en las unidades que necesita el abaco.
+# unidad | decena | centena | unidad de 1.000 | unidad de 10.000
+# unidad de 100.000
+# debe retornar todas estas variables.
+def descompone_numero():
+    
+#Funcion que muestra en pantalla el dibujo del abaco segun el numero ingresado
+def actualiza_tablero(numero):
+
+#funcion que aplica el punto separador de miles para mostrar
+#la cifra con claridad
+# *** debe retornar el resultado de la conversión
+def convertir_numero(numero):
+
+#Funcion que guarda el numero con punto en un diccionario
+# **Este debe retornar un diccionario
+def guardar_numero( ):
+
+    
