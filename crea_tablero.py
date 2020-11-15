2a8cca0751cde0507e80dbdc9d51e45acd1cad01
Pedro <pedrocid@example.com>
2020-11-15 14:15:01 -0300

    funcion actualiza tablero ok

--- crea_tablero.py
+++ crea_tablero.py
@@ -1,5 +1,5 @@
 
-def conversor_monto_abaco(monto):
+def cambia_digito(monto):
     if monto == 1:
         return 8
     elif monto == 2:
@@ -18,14 +18,11 @@ def conversor_monto_abaco(monto):
         return 1
     elif monto == 9:
         return 0
-
-#numero = int(input("Ingrese un numero"))
-
-#monto = conversor_monto_abaco(numero)
-monto = conversor_monto_abaco(4)
+    elif monto == 0:
+        return 10
 
 def crea_tablero():
-    lista_abaco = []
+    lista_abaco = {}
     contador = 0
     while contador!=6:
         abaco = [] #string que creará el dibujo
@@ -38,13 +35,36 @@ def crea_tablero():
                 abaco.append("  | |  ")    
             else:
                 abaco.append("#######")
-        lista_abaco.append(abaco)
+        lista_abaco[contador] = abaco
         contador +=1
     return lista_abaco
 
-lista_abaco = crea_tablero()
+def muestra_tablero(tablero):
+    for i in range(0,11):
+        for j in range(0,6):
+            print(tablero[j][i], sep="", end="")
+        print()
 
-for i in range(0,11):
-    for j in range(0,6):
-        print(lista_abaco[j][i], sep="", end="")
-    print()
+def actualiza(diccionario,numero):
+    cont_numero = 0
+    for clave in range(0,len(numero)):
+        abaco = []
+        for estado in range(0,11):
+            if estado == 0 :
+                abaco.append("   +   ") 
+            elif estado == 10:
+                abaco.append("   {}   ".format(numero[cont_numero]))    
+            elif estado >=1 and estado <=cambia_digito(numero[cont_numero]):
+                abaco.append("  | |  ")
+            else:
+                abaco.append(" ##### ")
+        cont_numero = cont_numero + 1
+        diccionario[clave] = abaco
+    return diccionario
+
+
+tablero = crea_tablero()
+muestra_tablero(tablero)
+print()
+tablero = actualiza(tablero,[1,9,8,7,6,5])
+muestra_tablero(tablero)
