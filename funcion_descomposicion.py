902086b69f71871316423bd389234a1687bc6646
joaoguzman <joaogg@ug.uchile.cl>
2020-11-16 10:05:59 -0300

    correccion numero de digitos

--- funcion_descomposicion.py
+++ funcion_descomposicion.py
@@ -4,7 +4,7 @@
 # unidad de 100.000
 # debe retornar todas estas variables.
 def descompone_numero(numero_original):
-    lista_digitos = []
+    lista_digitos = [0, 0, 0, 0, 0, 0]
     numero_original = str(numero_original)
     for digito in numero_original:
         lista_digitos.append(digito)
@@ -14,6 +14,6 @@ def descompone_numero(numero_original):
     mil = int(lista_digitos[-4])*1000
     diez_mil = int(lista_digitos[-5])*10000
     cien_mil = int(lista_digitos[-6])*100000
-    return print(lista_digitos, cien_mil, diez_mil, mil, centena, decena, unidad)
+    return print(cien_mil, diez_mil, mil, centena, decena, unidad)
 numero_original = int(input("Numero original: "))
 descompone_numero(numero_original)
